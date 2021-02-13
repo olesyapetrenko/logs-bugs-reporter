45d7cc6dedfe9918f687783ad915f5bc6e3097c3
Olesya <petrenco.olesya@gmail.com>
2021-02-13 14:53:38 +0200

    HW-61

--- logs-bugs-reporter/logs-db-populator/src/test/java/telran/logs/bugs/LogsDbPopulatorTest.java
+++ logs-bugs-reporter/logs-db-populator/src/test/java/telran/logs/bugs/LogsDbPopulatorTest.java
@@ -1,10 +1,9 @@
- package telran.logs.bugs;
+package telran.logs.bugs;
 
-import static org.assertj.core.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.util.Date;
 import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.*;
@@ -13,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.*;
 import org.springframework.context.annotation.Import;
-
 import org.springframework.messaging.support.GenericMessage;
 
 import telran.logs.bugs.dto.LogDto;
@@ -29,8 +27,7 @@ public class LogsDbPopulatorTest {
 	String bindingName;
 	@Autowired
 	InputDestination input;
-	@Autowired
-	OutputDestination output;
+	
 	@Autowired
 	LogsRepo logsRepo;
 
@@ -40,7 +37,7 @@ public class LogsDbPopulatorTest {
 	}
 
 	@Test
-	void takeLogDtoAndSaveNormal() {
+	void takeLogDtoAndSave() {
 		LogDto logDto = new LogDto(new Date(), LogType.NO_EXCEPTION, "artifact", 0, "");
 		sendLog(logDto);
 		List<LogDoc> logDocs = logsRepo.findAll();
@@ -48,45 +45,7 @@ public class LogsDbPopulatorTest {
 		assertEquals(logDto, logDocs.get(0).getLogDto());
 	}
 
-	@Test
-	void takeLogDtoAndNoSaveNoDate() {
-		LogDto logDto = new LogDto(null, LogType.NO_EXCEPTION, "artifact", 0, "");
-		sendLog(logDto);
-		testWrongLogDto();
-	}
-
-	private void testWrongLogDto() {
-		List<LogDoc> logDocs = logsRepo.findAll();
-		assertEquals(1, logDocs.size());
-		LogDto logDto = logDocs.get(0).getLogDto();
-		assertEquals(LogType.BAD_REQUEST_EXCEPTION, logDto.logType);
-		assertEquals(LogsDbPopulatorAppl.class.getName(), logDto.artifact);
-		assertEquals(0, logDto.responseTime);
-		assertFalse(logDto.result.isEmpty());
-		try {
-			String exceptionLogStr = new String(output.receive(0, bindingName).getPayload());
-			LOG.debug("exception log: {} sent to binding name: {} ", exceptionLogStr, bindingName);
-		} catch (Exception e) {
-			fail("No sent exception log to binding name: " + bindingName);
-		}
-		
-		
-	}
-
-	@Test
-	void takeLogDtoAndNoSaveNoLogType() {
-		LogDto logDto = new LogDto(new Date(), null, "artifact", 0, "");
-		sendLog(logDto);
-		testWrongLogDto();
-	}
-
-	@Test
-	void takeLogDtoAndNoSaveNoArtifact() {
-		LogDto logDto = new LogDto(new Date(), LogType.NO_EXCEPTION, "", 0, "");
-		sendLog(logDto);
-		testWrongLogDto();
-	}
-
+	
 	private void sendLog(LogDto logDto) {
 		
 		input.send(new GenericMessage<LogDto>(logDto));
