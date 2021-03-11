eaa83d1b64b0edb68691d72772a940b70516c204
Olesya <petrenco.olesya@gmail.com>
2021-03-11 19:05:08 +0200

    HW-70

--- logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/client/EmailProviderClient.java
+++ logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/client/EmailProviderClient.java
@@ -1,5 +1,4 @@
 package telran.logs.bugs.client;
-
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -9,33 +8,51 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class EmailProviderClient { 
+public class EmailProviderClient {
 	static Logger LOG = LoggerFactory.getLogger(EmailProviderClient.class);
 	RestTemplate restTemplate = new RestTemplate();
 	@Value("${app-url-assigner-mail:xxxx}")
 	String urlAssignerMail;
-	
-public String getEmailByArtifact(String artifact) {
-	//TODO communication with sync service for programmer's email
-	return "";
-}
-public String getAssignerMail() {
-	String res;
-	try {
-		ResponseEntity<String> response =
-				restTemplate.exchange(getUrlAssigner(), HttpMethod.GET, null, String.class);
-		res = response.getBody();
-	} catch (RestClientException e) {
-		res = "";
+	@Value("${app-url-programmer-mail:xxxx}")
+	String mailByArtifact;
+
+	public String getEmailByArtifact(String artifact) {
+		String urlMailProvider = getUrlMailArtifact(artifact);
+		String res = getMail(urlMailProvider);
+		LOG.debug("Programmer mail is {}", res);
+		return res;
 	}
-	LOG.debug("assigner email is {}", res);
-	return res;
-}
-private String getUrlAssigner() {
 
-	String res = urlAssignerMail + "/mail/assigner";
-	LOG.debug("URL for getting assigner mail is {}", res);
-	return res ;
-}
-}
+	public String getAssignerMail() {
+		String urlMailProvider = getUrlAssigner();
+		String res = getMail(urlMailProvider);
+		LOG.debug("assigner email is {}", res);
+		return res;
 
+	}
+
+	private String getMail(String urlMailProvider) {
+		String res;
+		try {
+			ResponseEntity<String> response = restTemplate.exchange(urlMailProvider, HttpMethod.GET, null,
+					String.class);
+			res = response.getBody();
+		} catch (RestClientException e) {
+			LOG.error("request to url {} thrown exception {}", urlMailProvider, e.getMessage());
+			res = "";
+		}
+		return res;
+	}
+
+	private String getUrlAssigner() {
+		String res = urlAssignerMail + "/mail/assigner";
+		LOG.debug("URL for getting assigner mail is {}", res);
+		return res;
+	}
+
+	private String getUrlMailArtifact(String artifact) {
+		String res = mailByArtifact + "/email/" + artifact;
+		LOG.debug("url for getting email by artifact is {}", res);
+		return res;
+	}
+}
