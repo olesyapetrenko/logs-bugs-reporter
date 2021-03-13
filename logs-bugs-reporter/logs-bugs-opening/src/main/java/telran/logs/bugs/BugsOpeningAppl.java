79ca1a7de57558c979ce4881e36b4f0ff691b8a4
Olesya <petrenco.olesya@gmail.com>
2021-03-13 17:02:38 +0200

    fix problem with module logs-bugs-opening

--- logs-bugs-reporter/logs-bugs-opening/src/main/java/telran/logs/bugs/BugsOpeningAppl.java
+++ logs-bugs-reporter/logs-bugs-opening/src/main/java/telran/logs/bugs/BugsOpeningAppl.java
@@ -2,7 +2,7 @@ package telran.logs.bugs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
 //@EntityScan("telran.logs.bugs.jpa.entities")
